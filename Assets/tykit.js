080576882f345055dc9067d7720956242cb8316d
刺猬不会跑 <108663173+axinkai622@users.noreply.github.com>
2025-02-01 14:46:47 +0800

    Add files via upload

---
+++ Assets/tykit.js
@@ -0,0 +1,3 @@
+//** 2024 Typecho Theme Development Framework ©️ Copyright By Tomoriゞ */
+const printLogo = () => {console.log("\n%c %s %c %s %c %s\n","color: #fff; background: #34495e; padding:5px 0;","基于Tykit框架","background: #fadfa3; padding:5px 0;","https://www.tykit.icu","color:#fff;background: #d6293e; padding:5px 0;","@Zirx_ ");};
+printLogo();
